3bb543062259da90c8c20c961d78c437db175559
jswini <jswinikoff@gmail.com>
2022-05-10 18:32:08 -0500

    first commit task 0 hls

---
+++ 0x01-ls/1_hls.c
@@ -0,0 +1,34 @@
+#include "hls.h"
+/**
+ * main - runs as if LS
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	DIR *dir;
+	struct dirent *read;
+
+	if (argc >= 1)
+	{
+		if (argv[1] == NULL)
+		{
+			dir = opendir(".");
+			while (((read = readdir(dir)) != NULL) && (strcmp(read->d_name,
+					".") != 0) && (strcmp(read->d_name, "..") != 0))
+				printf("%s ", read->d_name);
+		}
+		else
+		{
+			dir = opendir(argv[1]);
+			while ((read = readdir(dir)) != NULL)
+				printf("%s\n", read->d_name);
+		}
+		closedir(dir);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-ls/hls.h
@@ -0,0 +1,14 @@
+#ifndef LS_H
+#define LS_H
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pwd.h>
+#include <grp.h>
+#endif /*LS_H*/
