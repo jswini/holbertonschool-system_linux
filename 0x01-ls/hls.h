98d0468eee8dde63103ecc236c6602076f5d5ac4
jswini <jswinikoff@gmail.com>
2022-05-17 13:15:08 -0500

    updated other files for previous functionalization

--- 0x01-ls/hls.h
+++ 0x01-ls/hls.h
@@ -11,5 +11,10 @@
 #include <stdbool.h>
 #include <pwd.h>
 #include <grp.h>
+#include <errno.h>
 int _strcmp(char *s1, char *s2);
+char *str_concat(char *s1, char *s2);
+int _strlen(char *s);
+int chk_no_dir(DIR *dir, char *path);
+void print_dir(DIR *dir);
 #endif /*LS_H*/
