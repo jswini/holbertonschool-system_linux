a0c0feabcbf335df8f71781c6df0fadec57f3755
jswini <jswinikoff@gmail.com>
2022-05-17 12:22:24 -0500

    removed c++ style commenting

--- 0x01-ls/1_hls.c
+++ 0x01-ls/1_hls.c
@@ -11,30 +11,25 @@ int main(int argc, char *argv[])
 {
 	DIR *dir;
 	struct dirent *read;
-	//struct stat sb;
 
 	if (argc >= 1)
 	{
-		/* if (lstat(argv[1], &sb) == -1)
-            return(-1);
-		else */
+		if (argv[1] == NULL)
 		{
-			if (argv[1] == NULL)
-			{
-				dir = opendir(".");
-				while (((read = readdir(dir)) != NULL) && (_strcmp(read->d_name,
-						".") != 0) && (_strcmp(read->d_name, "..") != 0))
-						printf("%s  ", read->d_name);
-			}
-			else
-			{
-				dir = opendir(argv[1]);
-				while ((read = readdir(dir)) != NULL)
-					printf("%s\n", read->d_name);
-			}
-			closedir(dir);
+			dir = opendir(".");
+			while (((read = readdir(dir)) != NULL) && (_strcmp(read->d_name,
+					".") != 0) && (_strcmp(read->d_name, "..") != 0))
+					printf("%s  ", read->d_name);
 		}
+		else
+		{
+			dir = opendir(argv[1]);
+			while ((read = readdir(dir)) != NULL)
+				printf("%s\n", read->d_name);
+		}
+		closedir(dir);
 	}
+
 	printf("\n");
 	return (0);
 }
