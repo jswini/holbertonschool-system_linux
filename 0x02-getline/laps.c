28b1e93c0d829acd279acd368ee2d32a3888b3fe
jswini <jswinikoff@gmail.com>
2022-05-18 13:01:27 -0500

    sending to sandbox

--- 0x02-getline/laps.c
+++ 0x02-getline/laps.c
@@ -2,28 +2,59 @@
 
 void race_state(int *id, size_t size)
 {
+    static race_list *head;
+    race_list current = *head;
+
     if (size == 0)
         free_race(head);
     else
     {
         /*check id and create or increment cars*/
+        while (current.next != NULL)
+        {
+            if (current.next == id)
+                current.lap_num = current.lap_num + 1;
+            else
+                add_car(*head, id);
+        }
+        /*need to write print function*/
+        print_race(head);
     }
 }
 
-race_list add_car(list_t *head, const int id)
+race_list add_car(race_list head, const int id)
 {
     race_list current;
-    race_list next;
+    race_list new;
+    unsigned int i = 0;
 
-    if (current->car_num > id)
-        current = current->next;
+    if (!head || (head == NULL))
+        return (NULL);
+    new = malloc(sizeof(race_list));
+    if (new == NULL)
+        return (NULL);
+    if (*head == NULL)
+    {
+        new.car_num = id;
+        new.lap_num = 0;
+        new.next = current.next;
+        return (new);
+    }
     else
+    {
+        current = *head;
+        while (current != NULL)
+        if (current.car_num < id)
+            current = current.next;
+        else
         {
-            malloc(sizeof(race_list));
-            current->car_num = id;
-            current->lap_num - 0;
-            current->next = next;
+            current.car_num = new;
+            new.car_num = id;
+            new.lap_num = 0;
+            new.next = current.next;
         }
+    }
+    return (new);
 }
 
 void free_race(race_list *head)
@@ -36,11 +67,18 @@ void free_race(race_list *head)
     current = head;
     while (current != NULL)
     {
-        next = current->next;
-        free(current->car_id);
-        free(current->lap_num);
+        next = current.next;
         free(current);
         current = next;
     }
-    free next;
+    free (next);
+}
+
+void print_race(race_list *head)
+{
+    race_list current;
+
+    current = *head;
+    while (current->next != NULL)
+    printf("Car %d [%d laps]", current->car_num, current->lap_num);
 }
--- 0x02-getline/laps.h
+++ 0x02-getline/laps.h
@@ -1,5 +1,5 @@
-#ifndef LAPS.H
-#define LAPS.H
+#ifndef LAPS_H
+#define LAPS_H
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,9 +10,11 @@ typedef struct list_s
     /* data */
     int car_num;
     int lap_num;
-    struct list_s next;
+    struct list_s *next;
 } race_list;
 
 void race_state(int *id, size_t size);
-
+race_list add_car(race_list head, const int id);
+void free_race(race_list *head);
+void print_race(race_list *head);
 #endif /*LAPS_H*/
