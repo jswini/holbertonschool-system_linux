4212a16b8e864d01554d82768618426bf17dc29c
jswini <jswinikoff@gmail.com>
2023-05-16 12:21:47 -0500

    placeholder commit of racing cars task

---
+++ getline/laps.c
@@ -0,0 +1,12 @@
+#include "laps.h"
+
+/**
+ * race_state - This is the function that updates each car's state in the race
+ * @id: car identifier
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void race_state(int *id, size_t size)
+{}
---
+++ getline/laps.h
@@ -0,0 +1,9 @@
+#ifndef LAPS.H
+#define LAPS.H
+
+#include <stdio_ext.h>
+#include <stddef.h>
+
+void race_state(int *id, size_t size);
+
+#endif /*LAPS.H*/
